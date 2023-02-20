d5b46353fce20c5edf26b82b25c527cadfc4b1ac
kimkundad <kim.kundad@gmail.com>
2023-02-21 01:03:49 +0700

    kim update

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.template')
-
+<link rel="icon" type="image/png" sizes="32x32" href="{{ url('img/favicon-32x32.png') }}" />
 @section('title')
 หมวดหมู่และสินค้า วงษ์พาณิชย์รีไซเคิล ระยอง - wpnrayong
 @stop
