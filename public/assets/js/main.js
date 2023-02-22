2f51f0ea7a1599b7c6eabd8db4368e321834cb73
kimkundad <kim.kundad@gmail.com>
2023-02-22 15:58:23 +0700

    kim update

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -262,11 +262,11 @@
                     dataGap = el.data('owl-gap'),
                     dataNav = el.data('owl-nav'),
                     dataDots = el.data('owl-dots'),
-                    dataAnimateIn = el.data('slideInUp')
-                        ? el.data('slideInUp')
+                    dataAnimateIn = el.data('owl-animate-in')
+                        ? el.data('owl-animate-in')
                         : '',
-                    dataAnimateOut = el.data('slideOutUp')
-                        ? el.data('slideOutUp')
+                    dataAnimateOut = el.data('owl-animate-out')
+                        ? el.data('owl-animate-out')
                         : '',
                     dataDefaultItem = el.data('owl-item'),
                     dataItemXS = el.data('owl-item-xs'),
