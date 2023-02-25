cad9a12e13ceaf9f2d0844f12682e46aa99804bd
kimkundad <kim.kundad@gmail.com>
2023-02-25 17:24:02 +0700

    kim update

--- resources/views/layouts/inc-sidebar.blade.php
+++ resources/views/layouts/inc-sidebar.blade.php
@@ -3,13 +3,45 @@
     <li class="share top">
       <img src="{{ url('img/chat.png') }}" id="close_chat">
       <ul class="submenu">
-        <li><a href="#" class="facebook"><img src="{{ url('img/messenger.png') }}"></a></li>
+        <li><div id="fb-root"></div></li>
         <li><a href="{{ get_line() }}" class="twitter"><img src="{{ url('img/line.png') }}"></a></li>
         <li><a href="mailto: {{ get_email() }}" class="googlePlus"><img src="{{ url('img/email.png') }}"></a></li>
         <li><a href="tel:{{ get_phone() }}" class="instagram"><img src="{{ url('img/call.png') }}"></a></li>
       </ul>
     </li>
   </ul>
+
+  <!-- Messenger ปลั๊กอินแชท Code -->
+  <div id="fb-root"></div>
+
+  <!-- Your ปลั๊กอินแชท code -->
+  <div id="fb-customer-chat" class="fb-customerchat">
+  </div>
+
+  <script>
+    var chatbox = document.getElementById('fb-customer-chat');
+    chatbox.setAttribute("page_id", "262512967226949");
+    chatbox.setAttribute("attribution", "biz_inbox");
+  </script>
+
+  <!-- Your SDK code -->
+  <script>
+    window.fbAsyncInit = function() {
+      FB.init({
+        xfbml            : true,
+        version          : 'v16.0'
+      });
+    };
+
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  </script>
+
 <div id="back2top"><i class="icon icon-arrow-up"></i></div>
    <!-- <div class="ps-site-overlay"></div> -->
     <div class="ps-panel--sidebar" id="cart-mobile">
