88b249474bdb668f013abd2330d09a97757e1126
kimkundad <kim.kundad@gmail.com>
2023-04-05 12:03:37 +0700

    kim

--- resources/views/emails/contacts.blade.php
+++ resources/views/emails/contacts.blade.php
@@ -1,17 +1,42 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>wpnrayong.com</title>
-</head>
-<body>
-    <h1>{{$mydata['type']}}</h1>
-    <p>ชื่อผู้ติดต่อ {{$mydata['name']}} <br>
-        อีเมล {{$mydata['email']}} <br>
-        เบอร์ติดต่อ {{$mydata['phone']}} <br>
-    </p>
-  
-    <p>{{$mydata['messenger']}}</p>
-     
-    <p>Thank you</p>
-</body>
-</html>
+<style>html,body { padding: 0; margin:0; }</style>
+<div style="font-family:Arial,Helvetica,sans-serif; line-height: 1.5; font-weight: normal; font-size: 15px; color: #2F3044; min-height: 100%; margin:0; padding:0; width:100%; background-color:#edf2f7">
+	<table align="center" border="0" cellpadding="0" cellspacing="0" width="100%" style="border-collapse:collapse;margin:0 auto; padding:0; max-width:600px">
+		<tbody>
+			<tr>
+				<td align="center" valign="center" style="text-align:center; padding: 40px">
+					<a href="https://keenthemes.com" rel="noopener" target="_blank">
+						<img alt="logo wpnrayong" src="https://wpnrayong.com/img/wpn-logo.png" />
+					</a>
+				</td>
+			</tr>
+			<tr>
+				<td align="left" valign="center">
+					<div style="text-align:left; margin: 0 20px; padding: 40px; background-color:#ffffff; border-radius: 6px">
+						<!--begin:Email content-->
+						<div style="padding-bottom: 30px; font-size: 17px;">
+							<strong>{{$mydata['type']}}</strong>
+						</div>
+						<p>ชื่อผู้ติดต่อ {{$mydata['name']}} <br>
+                            อีเมล {{$mydata['email']}} <br>
+                            เบอร์ติดต่อ {{$mydata['phone']}} <br>
+                        </p>
+                    
+                        <p>{{$mydata['messenger']}}</p>
+                        
+                        <p>Thank you</p>
+						<!--end:Email content-->
+						<div style="padding-bottom: 10px">ข้อความจากเว้บไซต์
+						<br>ถึง บริษัท วงษ์พาณิชย์ รีไซเคิล ระยอง จำกัด
+						<tr>
+							<td align="center" valign="center" style="font-size: 13px; text-align:center;padding: 20px; color: #6d6e7c;">
+								<p>1/1 หมู่ 4 ต.นิคมพัฒนา อ.นิคมพัฒนา จ.ระยอง 21180</p>
+								<p>Copyright &copy;
+								<a href="https://wpnrayong.com" rel="noopener" target="_blank">wpnrayong.com</a>.</p>
+							</td>
+						</tr></br></div>
+					</div>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
