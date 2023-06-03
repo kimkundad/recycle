5e2136bba67699402a6bc34054a7fec85538bd4f
kimkundad <kim.kundad@gmail.com>
2023-06-04 00:49:27 +0700

    kim

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -166,7 +166,7 @@
                                         <div class="ps-product__content">
                                             @if($u->discount == 0)
                                             <p class="ps-product__price text-green">฿{{ number_format($u->amount, 2) }}
-                                                @if($u->unit_id !== 1 && $u->unit_id !== null)
+                                                @if($u->unit_id !== 3 && $u->unit_id !== null)
                                                 <b> {{ $u->name_unit }}</b>
                                                 @endif
                                             </p>
@@ -177,7 +177,7 @@
                                             @endphp
 
                                             <p class="ps-product__price sale">฿{{ number_format($u->amount-$discount, 2) }} <del>฿{{ number_format($u->amount, 2) }} </del>
-                                                @if($u->unit_id !== 1 && $u->unit_id !== null)
+                                                @if($u->unit_id !== 3 && $u->unit_id !== null)
                                                 <b> {{ $u->name_unit }}</b>
                                                 @endif
                                             </p>
@@ -187,7 +187,7 @@
                                         <div class="ps-product__content hover">
                                             @if($u->discount == 0)
                                             <p class="ps-product__price text-green">฿{{ number_format($u->amount, 2) }}
-                                                @if($u->unit_id !== 1 && $u->unit_id !== null)
+                                                @if($u->unit_id !== 3 && $u->unit_id !== null)
                                                 <b> {{ $u->name_unit }}</b>
                                                 @endif
                                             </p>
@@ -198,7 +198,7 @@
                                             @endphp
 
                                             <p class="ps-product__price sale">฿{{ number_format($u->amount-$discount, 2) }} <del>฿{{ number_format($u->amount, 2) }} </del>
-                                                @if($u->unit_id !== 1 && $u->unit_id !== null)
+                                                @if($u->unit_id !== 3 && $u->unit_id !== null)
                                                 <b> {{ $u->name_unit }}</b>
                                                 @endif
                                             </p>
