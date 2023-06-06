267d5a3b0a28a0958f46e23b71840ec2ee010510
kimkundad <kim.kundad@gmail.com>
2023-06-06 10:18:06 +0700

    kim

--- resources/views/admin/layouts/inc-header.blade.php
+++ resources/views/admin/layouts/inc-header.blade.php
@@ -190,9 +190,9 @@
                                 <!--end::Avatar-->
                                 <!--begin::Username-->
                                 <div class="d-flex flex-column">
-                                    <div class="fw-bold d-flex align-items-center fs-5">{{ Auth::user()->name }}</div>
-                                    <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Admin</span></div>
-                                    <a href="#" class="fw-semibold text-muted text-hover-primary fs-7">{{ Auth::user()->email }}</a>
+                                    <div class="fw-bold d-flex align-items-center fs-5">Max Smith
+                                    <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Pro</span></div>
+                                    <a href="#" class="fw-semibold text-muted text-hover-primary fs-7">max@kt.com</a>
                                 </div>
                                 <!--end::Username-->
                             </div>
