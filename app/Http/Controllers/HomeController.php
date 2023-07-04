3bacbab6de7b5e895b946eb47012ce7a09ed5138
kimkundad <kim.kundad@gmail.com>
2023-07-04 22:45:55 +0700

    kim update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -62,6 +62,7 @@ class HomeController extends Controller
         ->leftjoin('categories', 'categories.id',  'products.cat_id')
         ->leftjoin('unit_products', 'unit_products.id',  'products.unit_id')
         ->where('products.type_pro', 2)
+        ->where('products.status', 1)
         ->limit(12)
         ->get();
 
