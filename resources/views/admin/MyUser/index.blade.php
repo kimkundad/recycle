2392de2ce6c172c889bedff8b3704a1c0d4c50bf
kimkundad <kim.kundad@gmail.com>
2023-07-10 10:21:24 +0700

    kim

--- resources/views/admin/MyUser/index.blade.php
+++ resources/views/admin/MyUser/index.blade.php
@@ -68,7 +68,7 @@
                     <div class="card card-xl-stretch mb-5 mb-xl-8">
                         <div class="card-header border-0 pt-5">
                             <h3 class="card-title align-items-start flex-column">
-                                <span class="card-label fw-bold fs-3 mb-1">User ทั้งหมด</span>
+                                <span class="card-label fw-bold fs-3 mb-1">User ทั้งหมด </span>
                                 <span class="text-muted mt-1 fw-semibold fs-7"> User {{ count($objs) }}</span>
                             </h3>
                         </div>
@@ -115,7 +115,7 @@
                                             <td>
                                                 {{ $item->phone }}
                                             </td>
-                                            @if($item->name1 == 'superadmin')
+                                            @if(Auth::user()->roles[0]->name == 'superadmin')
                                             <td>
                                                 {{ $item->code_user }}
                                             </td>
