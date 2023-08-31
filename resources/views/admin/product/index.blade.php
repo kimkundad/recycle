cdef2c44ea134276fe613a1e7ca803780e8d0120
kimkundad <kim.kundad@gmail.com>
2023-08-31 17:30:04 +0700

    kim kundad

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -166,9 +166,9 @@
                                     <!--end::Table body-->
                                 </table>
                             </div>
-                            @if(count($objs) > 10)
+                        
                             @include('admin.pagination.default', ['paginator' => $objs])
-                            @endif
+                            
                         </div>
                     </div>
                     
