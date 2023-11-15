c52f4df8d11f93a53b224fcf7d7847988f7a8442
kimkundad <kim.kundad@gmail.com>
2023-11-15 21:52:26 +0700

    kim

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -59,10 +59,10 @@
 
                 @if(get_category())
                     @foreach(get_category() as $u)
-                        <div class="ps-product ps-product--inner">
-                            <a href="{{ url('category?id='.$u->id) }}" class="text-center">
+                        <div class="ps-product ps-product--inner photo_cat">
+                            <a href="{{ url('category?id='.$u->id) }}" class="text-center photo_cat">
                                 <div class="ps-block__number photo_cat">
-                                    <img class="img-fluid inheritpho" src="{{ url('images/wpnrayong/subcat/'.$u->image) }}"> 
+                                    <img class="img-fluid inheritpho " src="{{ url('images/wpnrayong/subcat/'.$u->image) }}"> 
                                 </div>
                             <p style="margin-top:10px">{{ $u->sub_name }}</p>
                             </a>
